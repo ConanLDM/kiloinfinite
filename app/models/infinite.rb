328be591e0b41618692e84134ae9b2e93bb76740
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-05-13 21:07:27 +0100

    Expanding the Infinite Model
    Purpose of this commit is purely to provide expansion and
    complexity to the Infinite model.
    
    The next branch will explore what can be done with expansion.
    
    Scopes have been added, along with attributes and pg_search
    implemntation into the Infinite model.

--- app/models/infinite.rb
+++ app/models/infinite.rb
@@ -1,2 +1,11 @@
 class Infinite < ApplicationRecord
+  include PgSearch::Model
+  multisearchable against: [:price]
+
+  validates :name, presence: true
+  validates :count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.0 }
+
+  scope :available, -> { where('count > 0') }
+  scope :expensive, -> { where('price > 100') }
 end
