f73e73855810eed354dcb878c25400c66d6c402d
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-06-17 20:35:22 +0100

    User Expanded with First and Last Name
    The user model and Devise sign up view have been expanded with the inclusion
    of first and last name fields, along with validations inserted for the already
    existing email and password fields, to ensure they are not extremely long in
    length and likely not an email/password.
    
    A method for the new inclusion of first and last name for a user has been
    setup in the user model as "name".
    
    Migration files have been created for both adding a name to a uuser and first
    and last name to a user, these have both been independently of each other
    before being migrated into the DB schema.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  validates :email, presence: true, length: { maximum: 200 }
+  validates :password, presence: true, length: { minimum: 6 }
+
   has_many :enigma_ratings
 end
